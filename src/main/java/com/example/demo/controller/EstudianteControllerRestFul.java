e92a24058ed842a4960ff02b678fee6398cbd2ea
AxeleoFC <axeleo2000@outlook.com>
2023-07-20 19:50:15 -0500

    Taller 31

--- src/main/java/com/example/demo/controller/EstudianteControllerRestFul.java
+++ src/main/java/com/example/demo/controller/EstudianteControllerRestFul.java
@@ -3,6 +3,9 @@ package com.example.demo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -17,6 +20,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.repository.modelo.Estudiante;
 import com.example.demo.service.IEstudianteService;
 
+import jakarta.xml.ws.Response;
+
 @RestController
 @RequestMapping("/estudiantes")
 public class EstudianteControllerRestFul {
@@ -26,8 +31,13 @@ public class EstudianteControllerRestFul {
 	
 	//GET
 	@GetMapping(path = "/{cedula}")
-	public Estudiante consultarPorCedula(@PathVariable String cedula) {
-		return this.estudianteService.consultarPorCedula(cedula);
+	public ResponseEntity<Estudiante> consultarPorCedula(@PathVariable String cedula) {
+		return ResponseEntity.status(227).body(this.estudianteService.consultarPorCedula(cedula));
+	}
+	
+	@GetMapping(path = "/status/{cedula}")
+	public ResponseEntity<Estudiante> consultarPorCedulaStatus(@PathVariable String cedula) {
+		return ResponseEntity.status(HttpStatus.OK).body(this.estudianteService.consultarPorCedula(cedula));
 	}
 	
 	@PostMapping
@@ -59,8 +69,13 @@ public class EstudianteControllerRestFul {
 	}
 	
 	@GetMapping
-	public List<Estudiante> buscarTodos() {
-		return this.estudianteService.buscarTodosEstudaintes();
+	public ResponseEntity<List<Estudiante>> buscarTodos() {
+		List<Estudiante> estu=this.estudianteService.buscarTodosEstudaintes();
+		System.out.println(estu);
+		HttpHeaders cabeceras=new HttpHeaders();
+		//.add(clve,valor)
+		cabeceras.add("detalle mensaje", "ciudadano consultados exitosamente");
+		return new ResponseEntity<>(estu,cabeceras,2020);
 	}
 	
 	@GetMapping(path = "/buscarTodosProvincia")
